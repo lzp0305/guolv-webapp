fc1bea71ba4ca0931c7d6dbf600d868fcd9cb736
李志鹏 <lzp0305@gmail.com>
2017-07-18 17:00:55 +0800

    2017-07-18

--- js/main.js
+++ js/main.js
@@ -43,5 +43,59 @@ function preview(file){
 	}
 }
 
+$(window).scroll(function() {
+	var show = $('.detail-tab').offset().top-$("body").scrollTop();
+	var xctsTop = $('#xcts-card').offset().top-$("body").scrollTop();
+	var jtxcTop = $('#jtxc-card').offset().top-$("body").scrollTop();
+	var fysmTop = $('#fysm-card').offset().top-$("body").scrollTop();
+	var zysxTop = $('#zysx-card').offset().top-$("body").scrollTop();
+	console.log(zysxTop);
+	if (show<=0) {
+		$('.detail-tab2').css({
+			'display': 'block'
+		});
+		$('.detail-tab').find('.tab-item-on').removeClass('tab-item-on');
+		$('.detail-tab').find('.tab-item-a-on').removeClass('tab-item-a-on');
+		$('.detail-tab').find('.tab-item:first-child').addClass('tab-item-on');
+		$('.detail-tab').find('#anchor-xcts').addClass('tab-item-a-on');
+	}else{
+		$('.detail-tab2').css({
+			'display': 'none'
+		});
+	}
 
+	if (zysxTop<=230) {
+		$('.detail-tab2').find('.tab-item-on').removeClass('tab-item-on');
+		$('.detail-tab2').find('.tab-item-a-on').removeClass('tab-item-a-on');
+		$('.detail-tab2').find('#anchor-zysx').addClass('tab-item-a-on');
+		$('.detail-tab2').find('#anchor-zysx').parent().addClass('tab-item-on');
+	} else if (fysmTop<=230) {
+		$('.detail-tab2').find('.tab-item-on').removeClass('tab-item-on');
+		$('.detail-tab2').find('.tab-item-a-on').removeClass('tab-item-a-on');
+		$('.detail-tab2').find('#anchor-fysm').addClass('tab-item-a-on');
+		$('.detail-tab2').find('#anchor-fysm').parent().addClass('tab-item-on');
+	} else if (jtxcTop<=230) {
+		$('.detail-tab2').find('.tab-item-on').removeClass('tab-item-on');
+		$('.detail-tab2').find('.tab-item-a-on').removeClass('tab-item-a-on');
+		$('.detail-tab2').find('#anchor-jtxc').addClass('tab-item-a-on');
+		$('.detail-tab2').find('#anchor-jtxc').parent().addClass('tab-item-on');
+	} else {
+		$('.detail-tab2').find('.tab-item-on').removeClass('tab-item-on');
+		$('.detail-tab2').find('.tab-item-a-on').removeClass('tab-item-a-on');
+		$('.detail-tab2').find('#anchor-xcts').addClass('tab-item-a-on');
+		$('.detail-tab2').find('#anchor-xcts').parent().addClass('tab-item-on');
+	}
+});
+
+
+$(document).ready(function(){
+	$('.tab-item').on('click', function(){
+		$(this).prevAll('.tab-item-on').removeClass('tab-item-on');
+		$(this).prevAll().find('a').removeClass('tab-item-a-on');
+		$(this).nextAll('.tab-item-on').removeClass('tab-item-on');
+		$(this).nextAll().find('a').removeClass('tab-item-a-on');
+		$(this).addClass('tab-item-on');
+		$(this).find('a').addClass('tab-item-a-on');
+	})
+});
 
