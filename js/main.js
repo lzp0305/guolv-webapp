583dc9c260d681488c3ab814707bba8837086672
李志鹏 <lzp0305@gmail.com>
2017-07-19 18:02:08 +0800

    2017-07-19

--- js/main.js
+++ js/main.js
@@ -17,6 +17,18 @@ $(document).on('ready', function(){
 	var listWidth = $('.business-list-title').width();
 	$('.business-list-route').css('width', listWidth);
 });
+$('.edit').on('click', function(){
+	setTimeout(function () {
+		var listWidth = $('.business-list-title').width();
+		$('.business-list-route').css('width', listWidth);
+	}, 300);
+});
+$('.cancel').on('click', function(){
+	setTimeout(function () {
+		var listWidth = $('.business-list-title').width();
+		$('.business-list-route').css('width', listWidth);
+	}, 300);
+});
 $(window).on('resize', function(){
 	setTimeout(function () {
 		var listWidth = $('.business-list-title').width();
@@ -89,7 +101,7 @@ $(window).scroll(function() {
 
 
 $(document).ready(function(){
-	$('.tab-item').on('click', function(){
+	$('.tab-item2').on('click', function(){
 		$(this).prevAll('.tab-item-on').removeClass('tab-item-on');
 		$(this).prevAll().find('a').removeClass('tab-item-a-on');
 		$(this).nextAll('.tab-item-on').removeClass('tab-item-on');
@@ -98,4 +110,65 @@ $(document).ready(function(){
 		$(this).find('a').addClass('tab-item-a-on');
 	})
 });
+
+
+$('.edit').click(function() {
+	$(this).css('display', 'none');
+	$('.cancel').css('display', 'block');
+	$('.business-list').css('padding', '0.32rem 0 0.32rem 1.152rem');
+	$('.cover-list').css('display', 'block');
+	$('.cover-list').toggle(function() {
+		$(this).css('background-image', 'url(icon/edit-on.png)');
+		$(this).parent().addClass('will-delete');
+	}, function() {
+		$(this).css('background-image', 'url(icon/edit-bg.png)');
+		$(this).parent().removeClass('will-delete');
+	});
+	$('.delete-item').css('display', 'block');
+
+});
+
+$('.delete-item').click(function(){
+	var mes = '确认删除？';
+	if ($('.usual-business-list li').hasClass('will-delete')) {
+		if (confirm(mes)==true) {
+			$('.will-delete').remove();
+			$('.cancel').css('display', 'none');
+			$('.edit').css('display', 'block');
+			$('.business-list').css('padding', '0.32rem 0 0.32rem 0');
+			$('.cover-list').css('display', 'none');
+			$('.cover-list').css('background-image', 'url(icon/edit-bg.png)');
+			$('.delete-item').css('display', 'none');
+		} else {
+			return false;
+		}
+	} else {
+		alert('没有选择项目');
+	}
+});
+
+
+
+$('.cancel').click(function(){
+	$(this).css('display', 'none');
+	$('.edit').css('display', 'block');
+	$('.business-list').css('padding', '0.32rem 0 0.32rem 0');
+	$('.cover-list').css('display', 'none');
+	$('.cover-list').css('background-image', 'url(icon/edit-bg.png)');
+	$('.delete-item').css('display', 'none');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
